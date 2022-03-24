7194fb76488e7f36663839afc67ae69579304114
mingeunoh6 <mingeunoh6@gmail.com>
2022-03-25 02:13:50 +0900

    comment_edit

--- client/src/components/Comment/CommentContent.js
+++ client/src/components/Comment/CommentContent.js
@@ -1,34 +1,67 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
-
-
+import useInput from "../../hooks/useInput";
+import axios from "axios";
 
 const CommentContent = (props) => {
+
+    const [editText, setEditText] = useState(props.comment)
     const user = useSelector((state) => state.user)
     const ref = useRef();
     const [isModalOpen, setModalOpen] = useState(false);
     const [modalFlag, setModalFlag] = useState(false)
+    const [isEdit, setIsEdit] = useState(false)
+
+
+
+
+    const onChangeEdit = (e) => {
+        setEditText(e.target.value)
+    }
+
+    const onCancel = (e) => {
+        setEditText(props.comment)
+        setIsEdit(false)
+    }
+
+    const onSubmitComment = (e) => {
+        e.preventDefault();
+        window.location.reload();
+    }
 
     useOnClickOutside(ref, () => setModalFlag(false));
     return (
         <div>
             <div>{props.author.displayName}</div>
-
+            {/* // 댓글쓴 사람과 로그인한 사람이 같을 경우  */}
             {props.author.uid === user.uid && (
                 <div>
                     <span onClick={() => setModalFlag(true)}>...</span>
                     {modalFlag && (
                         <div ref={ref}>
                             <div>delete</div>
-                            <div>edit</div>
+                            <div onClick={() => { setIsEdit(true) }}>edit</div>
                         </div>
                     )}
                 </div>)}
 
-            <div>{props.comment}</div>
+            {isEdit ?
+                <div>
 
+                    <form onSubmit={onSubmitComment}>
+                        <label htmlFor="comments">EDIT</label>
+                        <textarea
+                            id="comments"
+                            value={editText}
+                            onChange={onChangeEdit}
 
-
+                        />
+                        <button type='submit'>edit</button>
+                        <button type='button' onClick={onCancel}>cancel</button>
+                    </form>
+                </div> :
+                <div>{props.comment}</div>
+            }
         </div>
     );
 };
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,19 +2,20 @@
 import firebase, { initializeApp } from "firebase/compat/app";
 import { getAnalytics } from "firebase/analytics";
 import "firebase/compat/auth"
+import { firebaseOMG } from "./config/dev";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAzAmUMlL27OOR2kTnz2ZncOGoz46kvsPM",
-    authDomain: "omg-react.firebaseapp.com",
-    projectId: "omg-react",
-    storageBucket: "omg-react.appspot.com",
-    messagingSenderId: "181701958853",
-    appId: "1:181701958853:web:897715fb321855ca0824ac",
-    measurementId: "G-JKVDN2KXZD"
+    apiKey: firebaseOMG.apiKey,
+    authDomain: firebaseOMG.authDomain,
+    projectId: firebaseOMG.projectId,
+    storageBucket: firebaseOMG.storageBucket,
+    messagingSenderId: firebaseOMG.messagingSenderId,
+    appId: firebaseOMG.appId,
+    measurementId: firebaseOMG.measurementId
 };
 
 // Initialize Firebase
