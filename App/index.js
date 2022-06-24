8e923a15714f5e6b767dc4e0eff2319e91710994
mingeunoh6 <mingeunoh6@gmail.com>
2022-06-25 01:16:38 +0900

    newHeroku Setup

--- App/index.js
+++ App/index.js
@@ -6,14 +6,16 @@ const app = express();
 const port = process.env.PORT || 5000;
 const config = require("./server/config/key");
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "client", "build")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/image", express.static("./image"));
 app.use("/api/post", require("./server/Router/post"));
 app.use("/api/user", require("./server/Router/user"));
 app.use("/api/comment", require("./server/Router/comments"));
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => {
   mongoose
@@ -26,10 +28,10 @@ app.listen(port, () => {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 //무조건 index 로 들어오도록
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
